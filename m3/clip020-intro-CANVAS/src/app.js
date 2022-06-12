7a1fecd2b343d6d54147a404907de13aeb3bb8af
Peter Kellner <peter@peterkellner.net>
2022-06-12 15:22:50 -0700

    working

---
+++ m3/clip020-intro-CANVAS/src/app.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("solar-canvas");
+const context = canvas.getContext("2d", { alpha: false });
+
+context.fillStyle = "green";
+context.fillRect(10, 10, 150, 100);
+
---
+++ m3/clip050-DRAWSPACEOBJECT-MORE-DESTRUCTURE/src/app.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("solar-canvas");
+const context = canvas.getContext("2d", { alpha: false });
+
+context.fillStyle = "green";
+context.fillRect(10, 10, 150, 100);
+
+const sunPosition = {
+  x: 190,
+  y: 150,
+};
+
+const sunData = {
+  name: "Sun",
+  color: "yellow",
+  radius: 9,
+  x: sunPosition.x,
+  y: sunPosition.y,
+}
+
+function clear(context) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawStar(context, { color, name, radius, x, y }) {
+  context.beginPath();
+  context.fillStyle = color;
+  context.arc(x, y, radius, 0, Math.PI * 2, true); 
+  context.fill();
+  context.closePath();
+}
+
+clear(context);
+drawStar(context, sunData);
