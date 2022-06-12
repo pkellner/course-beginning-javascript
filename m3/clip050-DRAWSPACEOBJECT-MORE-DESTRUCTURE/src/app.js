5ef1857e707ed62e117fedfeb42190a6217a2c61
Peter Kellner <peter@peterkellner.net>
2022-06-12 16:26:54 -0700

    go

--- m3/clip050-DRAWSPACEOBJECT-MORE-DESTRUCTURE/src/app.js
+++ m3/clip050-DRAWSPACEOBJECT-MORE-DESTRUCTURE/src/app.js
@@ -1,9 +1,6 @@
 const canvas = document.getElementById("solar-canvas");
 const context = canvas.getContext("2d", { alpha: false });
 
-context.fillStyle = "green";
-context.fillRect(10, 10, 150, 100);
-
 const sunPosition = {
   x: 190,
   y: 150,
@@ -17,17 +14,40 @@ const sunData = {
   y: sunPosition.y,
 }
 
+const earthData = {
+  name: "Earth",
+  color: "blue",
+  radius: 5.1,
+  dist: 110,
+  angle: Math.PI / 2,
+  angleChangeRate: 0.013,
+}
+
 function clear(context) {
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawStar(context, { color, name, radius, x, y }) {
+function drawSpaceObject(context, color, name, radius, x, y) {
   context.beginPath();
   context.fillStyle = color;
   context.arc(x, y, radius, 0, Math.PI * 2, true); 
   context.fill();
   context.closePath();
+
+  context.font = "9px monospace";
+  context.fillStyle = "#ffffff";
+  context.textAlign = "center";
+  context.fillText(name, x, y - radius - 5);
+}
+
+function drawStar(context, { color, name, radius, x, y }) {
+  drawSpaceObject(context, color, name, radius, x, y);
 }
 
 clear(context);
 drawStar(context, sunData);
+
+const planetX = sunPosition.x + earthData.dist * 1;
+const planetY = sunPosition.y + earthData.dist * 0;
+drawSpaceObject(context, earthData.color, earthData.name, earthData.radius,
+  planetX, planetY);
