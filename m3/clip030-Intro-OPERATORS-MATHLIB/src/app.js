f23b756b353977ff754d536bab748f03e210dc99
Peter Kellner <peter@peterkellner.net>
2022-06-12 15:15:06 -0700

    wip

---
+++ m3/clip030-Intro-OPERATORS-MATHLIB/src/app.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("solar-canvas");
+const context = canvas.getContext("2d", { alpha: false });
+
+const sunPosition = {
+  x: 190,
+  y: 150,
+};
+
+const sunData = {
+  name: "Sun",
+  color: "yellow",
+  radius: 9,
+  x: sunPosition.x,
+  y: sunPosition.y,
+}
+
+function clear(context) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawStar(context, { color, name, radius, x, y }) {
+  context.beginPath();
+  context.fillStyle = color;
+  context.arc(x, y, radius, 0, 3.14159265 * 2, true);
+  context.fill();
+  context.closePath();
+}
+
+clear(context);
+drawStar(context, sunData);
