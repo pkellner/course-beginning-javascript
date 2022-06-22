026dd37908cf70ba58483206b10521959f6dd327
Peter Kellner <peter@peterkellner.net>
2022-06-21 18:27:52 -0700

    done!

---
+++ m7/m7-clip090DONE/src/lib/drawSpaceObject.js
@@ -0,0 +1,12 @@
+export function drawSpaceObject(context, color, name, radius, x, y) {
+  context.beginPath();
+  context.fillStyle = color;
+  context.arc(x, y, radius, 0, Math.PI * 2, true);
+  context.fill();
+  context.closePath();
+
+  context.font = '9px monospace';
+  context.fillStyle = '#ffffff';
+  context.textAlign = 'center';
+  context.fillText(name, x, y - radius - 5);
+}
--- m7/m7-clip090DONE/src/lib/planetFunction.js
+++ m7/m7-clip090DONE/src/lib/planetFunction.js
@@ -1,7 +1,6 @@
 import { drawOrbit } from './drawOrbit.js';
 import { drawSpaceObject } from './drawSpaceObject.js';
 import { moonFunction } from './moonFunction.js';
-
 export const planetFunction = (
   { x: sunX, y: sunY },
   { name, color, radius, dist, angle, angleChangeRate, moonDataList },
@@ -11,15 +10,17 @@ export const planetFunction = (
     moonDataList.map(function (moonData) {
       return moonFunction(moonData, context);
     }) : [];
+  console.log(moonFunctions);
   return {
     drawPlanet: function () {
       const planetX = sunX + dist * Math.sin(angle);
       const planetY = sunY + dist * Math.cos(angle);
+      debugger;
       drawSpaceObject(context, color, name, radius, planetX, planetY);
       moonFunctions.forEach(function (moonFunction) {
         moonFunction.drawMoon(planetX, planetY);
       })
-    },
+      },
     drawPlanetOrbit: function () {
       drawOrbit(context, sunX, sunY, dist);
     },
