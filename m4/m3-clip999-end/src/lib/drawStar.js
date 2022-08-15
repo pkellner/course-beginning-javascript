f12000d43e6ec92e7997726042406cece4ebe9b9
pkellner <peter@peterkellner.net>
2022-08-14 19:50:09 -0700

    updates

--- m4/m3-clip999-end/src/lib/drawStar.js
+++
@@ -1,5 +0,0 @@
-import { drawSpaceObject } from "./drawSpaceObject.js";
-
-export function drawStar(context, { color, name, radius, x, y }) {
-  drawSpaceObject(context, color, name, radius, x, y);
-}
